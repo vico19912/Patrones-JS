01819f8ca16b60464b7474b02ff6cb457fa0a960
vcorea <vcorea@senilix.com>
2020-08-19 12:36:18 -0600

    Ultimas funciones
    Agregue las funciones de agregar platillo y mostrar menú

--- js/namespace.js
+++ js/namespace.js
@@ -17,12 +17,29 @@ restaurApp.platillo = [
 //Funciones
 
 restaurApp.funciones = {
-    ordenar: id =>{
+    ordenar: id => {
         console.log(`
-            Tu platillo: ${restaurApp.platillo[id].platillo} se esta preparando
-        `);
+            Tu platillo: ${restaurApp.platillo[id].platillo} se esta preparando`);
+    },
+    agregarPaltillo: (platillo, precio) => {
+        const nuevo = {
+            platillo,
+            precio
+        }
+        restaurApp.platillo.push(nuevo);
+    },
+    mostrarMenu: platillos => {
+        console.log(`Bienvenido a nuestro menú: `);
+        platillos.forEach( (platillo, index)  => {
+            console.log( `${index} : ${platillo.platillo} $ ${platillo.precio}` )
+        });
     }
+
+
 }
 
 console.log(restaurApp);
+const { platillos } = restaurApp
+restaurApp.funciones.mostrarMenu(platillos);
+//restaurApp.funciones.ordenar(2);
 restaurApp.funciones.ordenar(2);
